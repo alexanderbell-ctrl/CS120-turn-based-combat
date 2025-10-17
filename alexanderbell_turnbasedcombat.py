5279de71290e339c0038d73bbfab1df177322c98
alexanderbell-ctrl <alexander.bell@bsu.edu>
2025-10-17 19:17:09 -0400

    Final game files

--- alexanderbell_turnbasedcombat.py
+++ alexanderbell_turnbasedcombat.py
@@ -1,3 +1,31 @@
 '''
+Alexander Bell
+Oct-17
 
+A turn-based combat game where two characters fight to the death based on their stats
 '''
+import random
+import alexanderbell_turnbasedcombat_tbc as tbc
+
+
+def main():
+    '''
+    char_stats: 0=name,1=hitPoints,2=hitChance,3=maxDamage,4=armor
+    '''
+    char1_stats = ["Jar Jar Binks",70,37,12,2]
+    char2_stats = ["Kermit the Frog",45,80,15,6]
+
+    char1 = tbc.character(char1_stats[0],char1_stats[1],char1_stats[2],char1_stats[3],char1_stats[4])
+    char2 = tbc.character(char2_stats[0],char2_stats[1],char2_stats[2],char2_stats[3],char2_stats[4])
+
+    char1.printStats()
+    char2.printStats()
+
+    tbc.character.fight(char1,char2)
+
+
+if __name__ == "__main__":
+	main()
+
+
+
--- alexanderbell_turnbasedcombat_tbc.py
+++ alexanderbell_turnbasedcombat_tbc.py
@@ -1,3 +1,92 @@
 '''
+Alexander Bell
+Oct-17
 
+A module for a turn-based combat game, which pits two characters against each other
 '''
+import random
+
+class character:
+    def __init__(self,name,hitPoints,hitChance,maxDamage,armor):
+        self.name = name
+        self.hitPoints = hitPoints
+        self.hitChance = hitChance
+        self.maxDamage = maxDamage
+        self.armor = armor
+
+    def printStats(self):
+        to_print = [self.hitPoints,self.hitChance,self.maxDamage,self.armor]
+        print(self.name)
+        print("================")
+        for item in to_print:
+            print(item)
+        print("\n")
+
+    def hit(char1,char2):
+        hit = random.randint(1,100)
+        if hit <= char2.hitChance:
+            damage = random.randint(1,char2.maxDamage)
+            damage_dealt = damage - char1.armor
+
+            print(f"{char2.name} hits {char1.name}...")
+            print(f"for {damage} hit points")
+            print(f"{char1.name}'s armor can reduce damage by {char1.armor} points")
+
+            if damage_dealt > 0:
+                return damage_dealt
+            else:
+                damage_dealt = 0
+                return damage_dealt
+        else:
+            print(f"{char2.name} misses {char1.name}...")
+            damage_dealt = 0
+            return damage_dealt
+
+    def fight(char1,char2):
+        keepGoing = True
+        health_char1 = char1.hitPoints
+        health_char2 = char2.hitPoints
+        while keepGoing:
+            new_round = input("\n\npress enter to go to the next round...")
+            if keepGoing == True:
+                damaged_char1 = character.hit(char1,char2)
+                health_char1 -= damaged_char1
+                char1.hitPoints = health_char1
+                character.printStats(char1)
+
+                if health_char1 <= 0:
+                    keepGoing = False
+                    print(f"{char2.name} wins!!!")
+            if keepGoing == True:
+                damaged_char2 = character.hit(char2,char1)
+                health_char2 -= damaged_char2
+                char2.hitPoints = health_char2
+                character.printStats(char2)
+
+                if health_char2 <= 0:
+                    keepGoing = False
+                    print(f"{char1.name} wins!!!")
+
+     
+
+
+def main():
+    char1 = character("test",8,99,8,2)
+    char2 = character("testing",8,1,8,2)
+
+    character.printStats(char1)
+    print("\n")
+    character.printStats(char2)
+    print("\n")
+
+    damage_1 = character.hit(char1,char2)
+    print(damage_1)
+    
+    damage_2 = character.hit(char2,char1)
+    print (damage_2)
+
+    character.fight(char1,char2)
+
+if __name__ == "__main__":
+	main()
+
